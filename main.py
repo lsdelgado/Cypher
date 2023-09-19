269b747d5676be706e9e1a3c2a16ac6e0eb8e138
Laura <la.san.del@hotmail.com>
2023-09-19 17:30:17 -0300

    Encryption functions update For loop (line 25) update

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ decrypted_text = ''
 encrypted_text = ''
 uppercase_alphabet = string.ascii_uppercase
 lowercase_alphabet = string.ascii_lowercase
-punctuation = string.punctuation
-whitespaces = string.whitespace
 digits = string.digits
 
 print("ENCRYPTION CYPHER - DO NOT USE DIACRITICS")
@@ -15,30 +13,25 @@ key = int(input("Encryption key: "))
 
 def lowercase_encrypt(char):
     encrypted_char_index = (lowercase_alphabet.index(char) + key) % 26
-    encrypted_text = encrypted_text + lowercase_alphabet[encrypted_char_index]
-    return (encrypted_text)
-
+    return(lowercase_alphabet[encrypted_char_index])
 def uppercase_encrypt(char):
     encrypted_char_index = (uppercase_alphabet.index(char) + key) % 26
-    encrypted_text = encrypted_text + uppercase_alphabet[encrypted_char_index]
-    return (encrypted_text)
-
+    return(uppercase_alphabet[encrypted_char_index])
 def digit_encrypt(char):
     encrypted_char_index = (digits.index(char) + key) % 10
-    encrypted_text = encrypted_text + digits[encrypted_char_index]
-    return (encrypted_text)
+    return(digits[encrypted_char_index])
 
 #ENCRYPTION
 for char in text:
 
     if char.islower():
-        lowercase_encrypt(char)
+        encrypted_text += lowercase_encrypt(char)
     elif char.isupper():
-        uppercase_encrypt(char)
+        encrypted_text += uppercase_encrypt(char)
     elif char.isspace():
         encrypted_text = encrypted_text + char
     elif char.isdigit():
-        encrypt_digit()
+        encrypted_text += digit_encrypt()
     else:
         encrypted_text = encrypted_text + char
 
